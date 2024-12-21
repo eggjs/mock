111876cd3f0780943ef01b769087d49ac517e0f5
fengmk2 <fengmk2@gmail.com>
2024-12-22 01:09:08 +0800

    agent and middleware

--- src/app/middleware/cluster_app_mock.ts
+++ src/app/middleware/cluster_app_mock.ts
@@ -1,11 +1,14 @@
-const debug = require('util').debuglog('egg-mock:middleware:cluster_app_mock');
-const is = require('is-type-of');
-const co = require('co');
+import { debuglog } from 'node:util';
+import { ContextDelegation, Next } from '@eggjs/core';
 
-module.exports = () => {
-  return function clusterAppMock(ctx, next) {
+const debug = debuglog('@eggjs/mock/app/middleware/cluster_app_mock');
+
+export default () => {
+  return function clusterAppMock(ctx: ContextDelegation, next: Next) {
     // use originalUrl to make sure other middlewares can't change request url
-    if (ctx.originalUrl !== '/__egg_mock_call_function') return next();
+    if (ctx.originalUrl !== '/__egg_mock_call_function') {
+      return next();
+    }
     debug('%s %s, body: %j', ctx.method, ctx.url, ctx.request.body);
     const { method, property, args, needResult } = ctx.request.body;
     if (!method) {
@@ -25,7 +28,7 @@ module.exports = () => {
       return;
     }
     if (property) {
-      if (!ctx.app[property] || typeof ctx.app[property][method] !== 'function') {
+      if (!ctx.app[property] || typeof (ctx.app as any)[property][method] !== 'function') {
         ctx.status = 422;
         ctx.body = {
           success: false,
@@ -54,7 +57,7 @@ module.exports = () => {
           // eslint-disable-next-line
           args[i] = eval(`(function() { return ${arg.value} })()`);
         } else if (arg.__egg_mock_type === 'error') {
-          const err = new Error(arg.message);
+          const err: any = new Error(arg.message);
           err.name = arg.name;
           err.stack = arg.stack;
           for (const key in arg) {
@@ -67,14 +70,13 @@ module.exports = () => {
       }
     }
 
-    const target = property ? ctx.app[property] : ctx.app;
-    let fn = target[method];
-    if (is.generatorFunction(fn)) fn = co.wrap(fn);
+    const target: any = property ? ctx.app[property] : ctx.app;
+    const fn = target[method];
     try {
       Promise.resolve(fn.call(target, ...args)).then(result => {
         ctx.body = needResult ? { success: true, result } : { success: true };
       });
-    } catch (err) {
+    } catch (err: any) {
       ctx.status = 500;
       ctx.body = { success: false, error: err.message };
     }
